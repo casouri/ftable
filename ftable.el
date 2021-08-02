96a0475871b1642582a0c0fb5a361a8af0dd5923
Yuan Fu <casouri@gmail.com>
2021-08-02 17:35:25 -0400

    Prepare for ELPA
    * ftable.el (ftable): Remove custom options.
    (ftable-fill, ftable-edit-cell, ftable-reformat): Add autoload cookie.

--- ftable.el
+++ ftable.el
@@ -1,8 +1,28 @@
 ;;; ftable.el --- Fill a table to fit in n columns      -*- lexical-binding: t; -*-
 
-;; Author: Yuan Fu <casouri@gmail.com>
+;; Copyright (C) 2019-2021 Free Software Foundation, Inc.
 
-;;; This file is NOT part of GNU Emacs
+;; Author: Yuan Fu <casouri@gmail.com>
+;; Maintainer: Yuan Fu <casouri@gmail.com>
+;; URL: https://github.com/casouri/ftable
+;; Version: 1.0
+;; Keywords: convenience, text, table
+;; Package-Requires: ((emacs "26.0"))
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;;
@@ -38,9 +58,7 @@
 
 ;;; Customization
 
-(defgroup ftable
-  '((ftable-fill-column custom-variable)
-    (ftable-recognize-table custom-variable))
+(defgroup ftable nil
   "Fill (auto-layout) tables."
   :group 'text)
 
@@ -494,6 +512,7 @@ Return a new string."
 
 ;;; Convenience
 
+;;;###autoload
 (defun ftable-fill ()
   "Fill the table (or paragraph) at point."
   (interactive)
@@ -512,6 +531,7 @@ Return a new string."
            #'table-recognize-region))
       (fill-paragraph))))
 
+;;;###autoload
 (defun ftable-edit-cell ()
   "Edit the cell at point."
   (interactive)
@@ -552,6 +572,7 @@ Return a new string."
          (when cell-p
            #'table-recognize-region))))))
 
+;;;###autoload
 (defun ftable-reformat (style)
   "Change box drawing STYLE for table at point.
 STYLE can be ’ascii or ’unicode."
